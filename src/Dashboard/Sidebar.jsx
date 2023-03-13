0e40718803f95aa285336b5053a17e6f47cc0019
Hamza Siddiqui <80615079+hamzapro305@users.noreply.github.com>
2023-03-13 16:57:36 +0500

    Sidebar Animation

--- src/Dashboard/Sidebar.jsx
+++ src/Dashboard/Sidebar.jsx
@@ -1,22 +1,25 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { Routes } from "../Data/Accessories";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { HiOutlineAcademicCap } from "react-icons/hi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const Sidebar = () => {
 	const navigate = useNavigate();
 	const location = useLocation();
 	const RouteHandler = (path) => navigate(path);
+	const [isOpen, setIsOpen] = useState(true);
 	return (
 		<motion.div
 			className="Dashboard-Sidebar"
 			initial={{}}
 			animate={{
-				width: "55px",
+				width: isOpen ? "300px" : "55px",
 			}}
 			exit={{}}
 		>
-			<div className="logo">
+			<div className="logo" onClick={() => RouteHandler('/')}>
 				<HiOutlineAcademicCap color="white" size={45} />
 			</div>
 			<div className="allRoutes">
@@ -26,15 +29,34 @@ const Sidebar = () => {
 						className="route MF"
 						key={x.path}
 					>
-						{x.icon}
+						<p>{x.icon}</p>
 						{location.pathname === x.path && (
 							<motion.div
 								className="left-border"
 								layoutId="leftBorder"
 							></motion.div>
 						)}
+						<AnimatePresence mode="wait">
+							{isOpen && (
+								<motion.div
+									exit={{ opacity: 0 }}
+									animate={{ opacity: 1 }}
+									initial={{ opacity: 0 }}
+									className="name"
+								>
+									{x.name}
+								</motion.div>
+							)}
+						</AnimatePresence>
 					</div>
 				))}
+			</div>
+			<div className="handler">
+				<GiHamburgerMenu
+					color="white"
+					fontSize={20}
+					onClick={() => setIsOpen((p) => !p)}
+				/>
 			</div>
 		</motion.div>
 	);
